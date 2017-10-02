d0e32bb1a699e2fe3494feec84b24f579934622e
vorozhba_sv <s.vorozhba@gmail.com>
2017-10-02 17:14:53 +0300

    uses 50/50 help -test

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -185,5 +185,20 @@ RSpec.describe GamesController, type: :controller do
       expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
       expect(response).to redirect_to(game_path(game))
     end
+
+    it 'uses 50/50 help' do
+      expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
+      expect(game_w_questions.fifty_fifty_used).to be_falsey
+
+      put :help, id: game_w_questions.id, help_type: :fifty_fifty
+      game = assigns(:game)
+
+      expect(game.finished?).to be_falsey
+      expect(game.fifty_fifty_used).to be_truthy
+      expect(game.current_game_question.help_hash[:fifty_fifty]).to include('d')
+      expect(game.current_game_question.help_hash[:fifty_fifty].size).to eq 2
+      expect(flash[:info]).to be
+      expect(response).to redirect_to(game_path(game))
+    end
   end
 end
